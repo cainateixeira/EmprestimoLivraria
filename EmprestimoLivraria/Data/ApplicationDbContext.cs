91ed1e7a955a71a28d43b43b5800adbffd25dbfb
Cainã Gabriel Teixeira <caina.teixeira@tecserver.com.br>
2023-11-21 17:16:19 -0300

    first-commit

---
+++ EmprestimoLivraria/Data/ApplicationDbContext.cs
@@ -0,0 +1,18 @@
+﻿using EmprestimoLivraria.Models;
+using Microsoft.EntityFrameworkCore;
+
+namespace EmprestimoLivraria.Data
+{
+    public class ApplicationDbContext : DbContext
+    {
+
+        public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options) : base(options)
+        {
+            
+        }
+
+
+        public DbSet<EmprestimosModel> Emprestimos { get; set; }
+
+    }
+}
